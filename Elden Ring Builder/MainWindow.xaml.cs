f2c409739fa9c5894bc8b6a2a006ef08efdccdf4
Oleksandr <aalexandr397@gmail.com>
2025-09-06 20:34:57 +0100

    basic UI

--- Elden Ring Builder/MainWindow.xaml.cs
+++ Elden Ring Builder/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -29,7 +30,84 @@ namespace Elden_Ring_Builder
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
-                this.DragMove(); // Позволяет двигать окно
+                this.DragMove();
+        }
+
+        private void about_app_Click(object sender, RoutedEventArgs e)
+        {
+            string aboutText = "Elden Ring Builder\n" +
+                               "Version: 1.0.0\n";
+            string descriptionText = "A simple application to help you build your character in Elden Ring.\n" +
+                                     "This application is not affiliated with FromSoftware or Bandai Namco Entertainment.";
+            MessageBox.Show(aboutText + "\n" + descriptionText);
+        }
+
+        private void steam_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "steam://open/main",
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Error, while steam opening");
+            }
+        }
+
+        private void author_steam_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string steamProfileUrl = "steam://openurl/https://steamcommunity.com/profiles/76561199220453620/";
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = steamProfileUrl,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Error, while steam opening");
+            }
+        }
+
+        private void github_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var url = "https://github.com/D1xer231";
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Error, while github opening");
+            }
+        }
+
+        private void author_github_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var url = "https://github.com/D1xer231/Elden-Ring-Builder";
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Error, while github opening");
+            }
         }
     }
 }
